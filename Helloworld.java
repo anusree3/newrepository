e7795fc15d474589114f2f4e1d1434a4ce01b01f
anusree3 <33477791+anusree3@users.noreply.github.com>
2018-03-29 14:58:43 +0000

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,19 +1,23 @@
-import javax.swing.*;
-public class Helloworld extends JFrame
-{
-public static void main(String[] args)
-{
-new Helloworld();
-}
-public Helloworld()
-{
-JPanel panel1 = new JPanel();
-JLabel label1 = new JLabel("Hello, world, this is my first Java  program on Ubuntu Linux");
-panel1.add(label1);
-this.add(panel1);
-this.setTitle("Hello world");
-this.setSize(500,500);
-this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-this.setVisible(true);
-}
-}
+
+
+
+public class ab{
+
+ 
+
+ public static void
+main(String args[])
+
+ {
+
+ 
+System.out.println("Hello world");
+
+ } 
+
+
+
+
+
+
+
